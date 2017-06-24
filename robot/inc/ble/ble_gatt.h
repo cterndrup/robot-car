440b6b3c7b31de1a641ff6029961b7349adf5bd2
Colin Terndrup <cterndrup@cs.ucla.edu>
2017-06-23 19:29:08 -0700

    refactor atCommands from global to static

--- robot/inc/ble/ble_gatt.h
+++ robot/inc/ble/ble_gatt.h
@@ -3,52 +3,6 @@
 #ifndef _BLE_GATT_H_
 #define _BLE_GATT_H_
 
-/* ------------------------ GLOBAL VARIABLES -------------------------------- */
-
-/* BLE GENERIC ATTRIBUTE PROFILE (GATT) AT-COMMAND STRINGS */
-
-/*!
- * Command Name: AT+GATTCLEAR
- * Description:  Clears any custom GATT services and characteristics that have
- *               been defined on the device
- */
-const char *atGattClear = "AT+GATTCLEAR";
-
-/*!
- * Command Name: AT+GATTADDSERVICE
- * Description:  Adds a new custom service definition to the device
- */
-const char *atGattAddService = "AT+GATTADDSERVICE";
-
-/*!
- * Command Name: AT+GATTADDCHAR
- * Description:  Adds a custom characteristic to the last service that was
- *               added to the peripheral (via AT+GATTADDSERVICE)
- */
-const char *atGattAddChar = "AT+GATTADDCHAR";
-
-/*!
- * Command Name: AT+GATTCHAR
- * Description:  Gets or sets the value of the specified custom GATT
- *               characteristic (based on the index ID returned when the
- *               characteristic was added to the system via AT+GATTADDCHAR)
- */
-const char *atGattChar = "AT+GATTCHAR";
-
-/*!
- * Command Name: AT+GATTLIST
- * Description:  Lists all custom GATT services and characteristics that have
- *               been defined on the device
- */
-const char *atGattList = "AT+GATTLIST";
-
-/*!
- * Command Name: AT+GATTCHARRAW
- * Description:  This read only command reads binary (instead of ASCII) data
- *               from a characteristic
- */
-const char *atGattCharRaw = "AT+GATTCHARRAW";
-
 /* ------------------------ MACROS AND DEFINES ------------------------------ */
 
 #define BLE_GATT_MAX_SERVICES                (10)
--- robot/src/ble/ble.c
+++ robot/src/ble/ble.c
@@ -14,21 +14,21 @@
 /* ------------------------ EXTERNS ----------------------------------------- */
 
 /*!
- * External definition of semaphore for synchronizing access to sdep buffers
+ * External declaration of semaphore for synchronizing access to sdep buffers
  */
 extern semaphore_t sdepBufferSemaphore;
 
 /*!
- * External definition of sdepRespBuffer
+ * External declaration of sdepRespBuffer
  */
 extern SDEP_MSG_BUFFER sdepRespBuffer;
 
 /*!
- * External definition of the Car object
+ * External declaration of the Car object
  */
 extern Car car;
 
-/* ------------------------ GLOBAL VARIABLES -------------------------------- */
+/* ------------------------ STATIC VARIABLES -------------------------------- */
 
 /*!
  * Declaration of RobotDriveService and its characteristics
@@ -36,6 +36,480 @@ extern Car car;
 BLE_GATT_SERVICE RobotDriveService;
 BLE_GATT_CHAR    RobotDriveCharSpeed;
 BLE_GATT_CHAR    RobotDriveCharDirection;
+
+/*!
+ * Definition of BLE module name
+ */
+static const char *bleDeviceName = "BluefruitLE";
+
+/*!
+ * Definition of empty BLE command payload
+ */
+static const char bleCmdEmptyPayload[1] = {'\0'};
+
+
+
+/* STANDARD AT-COMMAND STRINGS */
+
+/*!
+ * Command Name: AT
+ * Description:  Acts as ping to check if in command mode
+ */
+static const char *at  = "AT";
+
+/*!
+ * Command Name: ATI
+ * Description:  Displays basic info about the Bluefruit module
+ */
+static const char *ati = "ATI";
+
+/*!
+ * Command Name: ATZ
+ * Description:  Performs a system reset
+ */
+static const char *atz = "ATZ";
+
+/*!
+ * Command Name: ATE
+ * Description:  Enables/disables echo of input chars with AT parser
+ */
+static const char *ate = "ATE";
+
+/*!
+ * Command Name: PPP (+++)
+ * Description:  Dynamically switches between DATA/CMD mode...
+ */
+static const char *ppp = "+++";
+
+
+
+/* GENERAL PURPOSE AT-COMMAND STRINGS */
+
+/*!
+ * Command Name: AT+FACTORYRESET
+ * Description:  Clear any user config data from NVM: and performs factory reset
+ *               before resetting module
+ */
+static const char *atFactoryReset = "AT+FACTORYRESET";
+
+/*!
+ * Command Name: AT+DFU
+ * Description:  Forces module into DFU mode, allowing over the air firmware
+ *               updates using dedicate DFU app
+ */
+static const char *atDfu = "AT+DFU";
+
+/*!
+ * Command Name: AT+HELP
+ * Description:  Displays comma-separated list of all AT parser commands
+ *               available on the system
+ */
+static const char *atHelp = "AT+HELP";
+
+/*!
+ * Command Name: AT+NVMWRITE
+ * Description:  Writes data to the 256 byte user non-volatile memory region
+ */
+static const char *atNvmWrite = "AT+NVMWRITE";
+
+/*!
+ * Command Name: AT+NVMREAD
+ * Description:  Reads data form the 256 byte user non-volatile memory region
+ */
+static const char *atNvmRead = "AT+NVMREAD";
+
+/*!
+ * Command Name: AT+MODESWITCHEN
+ * Description:  Enables of disables mode switches via the '+++' command on the
+ *               BLE peripheral of BLE UART side of the connection
+ */
+static const char *atModeSwitchEn = "AT+MODESWITCHEN";
+
+
+
+/* HARDWARE AT-COMMAND STRINGS */
+
+/*!
+ * Command Name: AT+BAUDRATE
+ * Description:  Changes the baud rate used by the HW UART peripheral on the
+ *               nRF51822
+ */
+static const char *atBaudRate = "AT+BAUDRATE";
+
+/*!
+ * Command Name: AT+HWADC
+ * Description:  Performs an ADC conversion on the specified ADC pin
+ */
+static const char *atHwAdc = "AT+HWADC";
+
+/*!
+ * Command Name: AT+HWGETDIETEMP
+ * Description:  Gets the temperature in degree celcius of the BLE module's die
+ */
+static const char *atHwGetDieTemp = "AT+HWGETDIETEMP";
+
+/*!
+ * Command Name: AT+HWGPIO
+ * Description:  Gets or sets the value of the specified GPIO pin (depending on
+ *               the mode of the pin
+ */
+static const char *atHwGpio = "AT+HWGPIO";
+
+/*!
+ * Command Name: AT+HWGPIOMODE
+ * Description:  This will set the mode for the specified GPIO pin
+ */
+static const char *atHwGpioMode = "AT+HWGPIOMODE";
+
+/*!
+ * Command Name: AT+HWI2CSCAN
+ * Description:  Scans the I2C bus to try to detect any connected I2C devices
+ *               and returns addresses of devices that were found during scan
+ *               process
+ */
+static const char *atHwI2cScan = "AT+HWI2CSCAN";
+
+/*!
+ * Command Name: AT+HWVBAT
+ * Description:  Returns the main power supply voltage level in millivolts
+ */
+static const char *atHwVBat = "AT+HWVBAT";
+
+/*!
+ * Command Name: AT+HWRANDOM
+ * Description:  Generates a random 32-bit number using the HW random number
+ *               generator on the nRF51822 (based on white noise)
+ */
+static const char *atHwRandom = "AT+HWRANDOM";
+
+/*!
+ * Command Name: AT+HWMODELED
+ * Description:  Allows oyu to override hte default behavior of the MODE led
+ *               (which indicates the operaing mode by default)
+ */
+static const char *atHwModeLed = "AT+HWMODELED";
+
+/*!
+ * Command Name: AT+UARTFLOW
+ * Description:  Enables or disable hardware flow control (CTS + RTS) on the
+ *               UART peripheral block of the nRF51822
+ */
+static const char *atUartFlow = "AT+UARTFLOW";
+
+
+
+/* BLE GENERIC AT-COMMAND STRINGS */
+
+/*!
+ * Command Name: AT+BLEPOWERLEVEL
+ * Description:  Gets or sets the current transmit power level for the module's
+ *               radio (higher transmit power equals better range, lower
+ *               transmit power equals better battery life
+ */
+static const char *atBlePowerLevel = "AT+BLEPOWERLEVEL";
+
+/*!
+ * Command Name: AT+BLEGETADDRTYPE
+ * Description:  Gets the address type (for the 48-bit BLE device address)
+ */
+static const char *atBleGetAddrType = "AT+BLEGETADDRTYPE";
+
+/*!
+ * Command Name: AT+BLEGETADDR
+ * Description:  Gets the 48-bit BLE device address
+ */
+static const char *atBleGetAddr = "AT+BLEGETADDR";
+
+/*!
+ * Command Name: AT+BLEGETPEERADDR
+ * Description:  Gets the 48-bit address of the peer (central) device we are
+ *               connected to
+ */
+static const char *atBleGetPeerAddr = "AT+BLEGETPEERADDR";
+
+/*!
+ * Command Name: AT+BLEGETRSSI
+ * Description:  Gets the RSSI (Received Signal Strength Indicator), which can
+ *               be used to estimate the reliability of data transmission
+ *               between two devices (the lower the number the better)
+ */
+static const char *atBleGetRssi = "AT+BLEGETRSSI";
+
+
+
+/* BLE SERVICES AT-COMMAND STRINGS */
+
+/*!
+ * Command Name: AT+BLEUARTTX
+ * Description:  This command will transmit the specified text message out via
+ *               the UART Service while you are running in Command Mode
+ */
+static const char *atBleUartTx = "AT+BLEUARTTX";
+
+/*!
+ * Command Name: AT+BLEUARTTXF
+ * Decription:   This is a convenience function that serves the same purpose as
+ *               AT+BLEUARTTX, but data is immediately sent in a single BLE 
+ *               packet
+ */
+static const char *atBleUartTxF = "AT+BLEUARTTXF";
+
+/*!
+ * Command Name: AT+BLEUARTRX
+ * Description:  This command will dump the UART service's RX buffer to the
+ *               display if any data has been received from the UART service
+ *               while running in Command Mode
+ */
+static const char *atBleUartRx = "AT+BLEUARTRX";
+
+/*!
+ * Command Name: AT+BLEUARTFIFO
+ * Description:  This command will return the free space available in the BLE
+ *               UART TX and RX FIFOs
+ */
+static const char *atBleUartFifo = "AT+BLEUARTFIFO";
+
+/*!
+ * Command Name; AT+BLEKEYBOARDEN
+ * Description:  This command will enable GATT over HID keyboard support, which
+ *               allows you to emulate a keyboard on supported iOS and Android
+ *               devices
+ */
+static const char *atBleKeyboardEn = "AT+BLEKEYBOARDEN";
+
+/*!
+ * Command Name: AT+BLEKEYBOARD
+ * Description:  Sends text data over the BLE keyboard interface
+ */
+static const char *atBleKeyboard = "AT+BLEKEYBOARD";
+
+/*!
+ * Command Name: AT+BLEKEYBOARDCODE
+ * Description:  Sends a raw hex sequence of USB HID keycodes to the BLE
+ *               keyboard interface including key modifiers and up to six alpha-
+ *               numeric characters
+ */
+static const char *atBleKeyboardCode = "AT+BLEKEYBOARDCODE";
+
+/*!
+ * Command Name: AT+BLEHIDEN
+ * Description:  This command will enable GATT over HID support, which allows
+ *               you to emulate a keyboard, moust or mediat control on supported
+ *               iOS, Android, OSX and Windows 10 devices
+ */
+static const char *atBleHidEn = "AT+BLEHIDEN";
+
+/*!
+ * Command Name: AT+BLEHIDMOUSEMOVE
+ * Description:  Moves the HID mouse or scroll wheel position the specified
+ *               number of ticks
+ */
+static const char *atBleHidMouseMove = "AT+BLEHIDMOUSEMOVE";
+
+/*!
+ * Command Name: AT+BLEHIDMOUSEBUTTON
+ * Description:  Manipulates the HID mouse buttons via the specific string(s)
+ */
+static const char *atBleHidMouseButton = "AT+BLEHIDMOUSEBUTTON";
+
+/*!
+ * Command Name: AT+BLEHIDCONTROLKEY
+ * Description:  Sends HID media control commands for the bonded device
+ */
+static const char *atBleHidControlKey = "AT+BLEHIDCONTROLKEY";
+
+/*!
+ * Command Name: AT+BLEHIDGAMEPADEN
+ * Description:  Enables HID gamepad support in the HID service
+ */
+static const char *atBleHidGamePadEn = "AT+BLEHiDGAMEPADEN";
+
+/*!
+ * Command Name: AT+BLEHIDGAMEPAD
+ * Description:  Sends a specific HID gamepad payload out over BLE
+ */
+static const char *atBleHidGamePad = "AT+BLEHIDGAMEPAD";
+
+/*!
+ * Command Name; AT+BLEMIDIEN
+ * Description:  Enables or disables the BLE MIDI service
+ */
+static const char *atBleMidiEn = "AT+BLEMIDIEN";
+
+/*!
+ * Command Name: AT+BLEMIDIRX
+ * Description:  Reads an incoming MIDI character array from the buffer
+ */
+static const char *atBleMidiRx = "AT+BLEMIDIRX";
+
+/*!
+ * Command Name: AT+BLEMIDITX
+ * Description:  Sends a MIDI event to host
+ */
+static const char *atBleMidiTx = "AT+BLEMIDITX";
+
+/*!
+ * Command Name: AT+BLEBATTEN
+ * Description:  Enables the Battery Service following the definition from the
+ *               Bluetooth SIG
+ */
+static const char *atBleBattEn = "AT+BLEBATTEN";
+
+/*!
+ * Command Name: AT+BLEBATTVAL
+ * Description:  Sets the current battery level in percentage (0..100) for the
+ *               Battery Service
+ */
+static const char *atBleBattVal = "AT+BLEBATTVAL";
+
+
+
+/* DEBUG AT-COMMANDS */
+/*! 
+ * NOTE: Debug commands should be used with care since they can easily lead to 
+ *       HardFault on ARM Cortex-M core of BLE module
+ */
+
+/*!
+ * Command Name: AT+DBGMEMRD
+ * Description:  Displays the raw memory contents at the specified address
+ */
+static const char *atDbgMemRd = "AT+DBGMEMRD";
+
+/*!
+ * Command Name: AT+DBGNVMRD
+ * Description:  Displays the raw contents of the config data section of non-
+ *               volatile memory
+ */
+static const char *atDbgNvmRd = "AT+DBGNVMRD";
+
+/*!
+ * Command Name: AT+DBGSTACKSIZE
+ * Description:  Returns the current stack size, to help detect stack overflow
+ *               or detect stack memory usage when optimizing memory usage on
+ *               the system
+ */
+static const char *atDbgStackSize = "AT+DBGSTACKSIZE";
+
+/*!
+ * Command Name: AT+DBGSTACKDUMP
+ * Description:  Dumps the current stack contents -- Unused sections of stack
+ *               memory are filled with '0xCAFEFOOD' to help determine where
+ *               stack usage stops
+ */
+static const char *atDbgStackDump = "AT+DBGSTACKDUMP";
+
+
+
+/* BLE GENERIC ACCESS PROFILE (GAP) AT-COMMANDS */
+
+/*!
+ * Command Name: AT+GAPCONNECTABLE
+ * Description:  This command can be used to prevent the device from being
+ *               'connectable'
+ */
+static const char *atGapConnectAble = "AT+GAPCONNECTABLE";
+
+/*!
+ * Command Name: AT+GAPGETCONN
+ * Description:  Displays the current connection status
+ */
+static const char *atGapGetConn = "AT+GAPGETCONN";
+
+/*!
+ * Command Name: AT+GAPDISCONNECT
+ * Description:  Disconnects to the external device if we are currently
+ *               connected
+ */
+static const char *atGapDisconnect = "AT+GAPDISCONNECT";
+
+/*!
+ * Command Name: AT+GAPDEVNAME
+ * Description:  Gets or sets the device name, which is included in the
+ *               advertising payload for the Bluefruit LE module
+ */
+static const char *atGapDevName = "AT+GAPDEVNAME";
+
+/*!
+ * Command Name: AT+GAPDELBONDS
+ * Description:  Deletes bonding information stored on the Bluefruit LE module
+ */
+static const char *atGapDelBonds = "AT+GAPDELBONDS";
+
+/*!
+ * Command Name: AT+GAPINTERVALS
+ * Description:  Gets or sets the varios advertising and connection intervals
+ *               for the Bluefruit LE module
+ */
+static const char *atGapIntervals = "AT+GAPINTERVALS";
+
+/*!
+ * Command Name: AT+GAPSTARTADV
+ * Description:  Causes the Bluefruit LE module to start transmitting 
+ *               advertising packets if this isn't already the case
+ */
+static const char *atGapStartAdv = "AT+GAPSTARTADV";
+
+/*!
+ * Command Name: AT+GAPSTOPADV
+ * Description:  Stops advertising packets from being transmitted by the
+ *               Bluefruit LE module
+ */
+static const char *atGapStopAdv = "AT+GAPSTOPADV";
+
+/*!
+ * Command Name: AT+GAPSETADVDATA
+ * Description:  Sets the raw advertising data payload to the specified byte
+ *               array (overriding the normal advertising data), following the
+ *               guidelines in the Bluetooth 4.0 or 4.1 Core Specification
+ */
+static const char *atGapSetAdvData = "AT+GAPSETADVDATA";
+
+
+
+/* BLE GENERIC ATTRIBUTE PROFILE (GATT) AT-COMMAND STRINGS */
+
+/*!
+ * Command Name: AT+GATTCLEAR
+ * Description:  Clears any custom GATT services and characteristics that have
+ *               been defined on the device
+ */
+static const char *atGattClear = "AT+GATTCLEAR";
+
+/*!
+ * Command Name: AT+GATTADDSERVICE
+ * Description:  Adds a new custom service definition to the device
+ */
+static const char *atGattAddService = "AT+GATTADDSERVICE";
+
+/*!
+ * Command Name: AT+GATTADDCHAR
+ * Description:  Adds a custom characteristic to the last service that was
+ *               added to the peripheral (via AT+GATTADDSERVICE)
+ */
+static const char *atGattAddChar = "AT+GATTADDCHAR";
+
+/*!
+ * Command Name: AT+GATTCHAR
+ * Description:  Gets or sets the value of the specified custom GATT
+ *               characteristic (based on the index ID returned when the
+ *               characteristic was added to the system via AT+GATTADDCHAR)
+ */
+static const char *atGattChar = "AT+GATTCHAR";
+
+/*!
+ * Command Name: AT+GATTLIST
+ * Description:  Lists all custom GATT services and characteristics that have
+ *               been defined on the device
+ */
+static const char *atGattList = "AT+GATTLIST";
+
+/*!
+ * Command Name: AT+GATTCHARRAW
+ * Description:  This read only command reads binary (instead of ASCII) data
+ *               from a characteristic
+ */
+static const char *atGattCharRaw = "AT+GATTCHARRAW";
 
 /* ------------------------ STATIC FUNCTIONS -------------------------------- */
 
